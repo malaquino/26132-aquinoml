7ab24c39267804ed2ef65b9f885086cf0347c954
Laura Aquino <malaquino@gmail.com>
2026-07-13 01:29:53 -0300

    Modificaciones para vercel

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.use(cors({
 }));
 
 app.use(express.json());
-//app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   console.log(`Datos recibidos: ${req.method} ${req.url}`);
